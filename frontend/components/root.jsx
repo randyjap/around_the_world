f5b5414205aa190981d3e5c3dd299a78944d52f8
randyjap <randyjap@gmail.com>
2017-02-24 16:44:46 -0800

    Add root, scss structure, static page structure, javascript tag in application layout

---
+++ frontend/components/root.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import App from './app';
+import SessionFormContainer from './session/session_form_container';
+import GreetingContainer from './greeting/greeting_container';
+
+const Root = ({ store }) => {
+  // const _ensureLoggedIn = (nextState, replace) => {
+  //   const currentUser = store.getState().session.currentUser;
+  //   if (!currentUser) {
+  //     replace('login');
+  //   }
+  // };
+  //
+  // const _redirectIfLoggedIn = (nextState, replace) => {
+  //   const currentUser = store.getState().session.currentUser;
+  //   if (currentUser) {
+  //     replace('/search');
+  //   }
+  // };
+  //
+  // const _goToSearchIfLoggedIn = (nextState, replace) => {
+  //   const currentUser = store.getState().session.currentUser;
+  //   if (currentUser) {
+  //     replace('search');
+  //   }
+  // };
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory } onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ GreetingContainer } />
+          <Route path="login" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
+          <Route path="signup" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
+        </Route>
+      </Router>
+    </Provider>
+  );
+};
+
+export default Root;
